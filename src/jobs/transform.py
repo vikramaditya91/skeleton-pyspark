b372bc3086d8e64a45614040904a09cf198894dd
frederikdesmedt <frederik@dataroots.io>
2021-09-11 16:30:33 +0200

    black reformatting

--- src/jobs/transform.py
+++ src/jobs/transform.py
@@ -7,7 +7,14 @@ from pyspark.sql.types import IntegerType, FloatType
 
 
 def clean_df_nulls(df):
-    return df.na.drop(subset=["home_team_goal", "away_team_goal", "home_team_api_id", "away_team_api_id"])
+    return df.na.drop(
+        subset=[
+            "home_team_goal",
+            "away_team_goal",
+            "home_team_api_id",
+            "away_team_api_id",
+        ]
+    )
 
 
 def column_replace_name(df, orig: str, final: str):
@@ -23,14 +30,17 @@ def transform_match_df(match_df_raw: DataFrame) -> DataFrame:
     reduced_match_df = reduced_match_df.withColumn(
         "result",
         func.when(
-            reduced_match_df.home_team_goal.cast(IntegerType()) > reduced_match_df.away_team_goal.cast(IntegerType()),
-            func.lit("WIN")
-        ).when(
-            reduced_match_df.home_team_goal.cast(IntegerType()) == reduced_match_df.away_team_goal.cast(IntegerType()),
-            func.lit("TIE")
-        ).otherwise(
-            func.lit("LOSE")
-        ))
+            reduced_match_df.home_team_goal.cast(IntegerType())
+            > reduced_match_df.away_team_goal.cast(IntegerType()),
+            func.lit("WIN"),
+        )
+        .when(
+            reduced_match_df.home_team_goal.cast(IntegerType())
+            == reduced_match_df.away_team_goal.cast(IntegerType()),
+            func.lit("TIE"),
+        )
+        .otherwise(func.lit("LOSE")),
+    )
     reduced_match_df = reduced_match_df.na.drop(subset=["result"])
 
     home_df = column_replace_name(reduced_match_df, "home", "this")
@@ -46,23 +56,28 @@ def transform_match_df(match_df_raw: DataFrame) -> DataFrame:
 
 def get_team_win_ratio(transformed_match_df):
     return (
-        transformed_match_df
-            .groupBy("this_team_api_id")
-            .agg(
-                func.sum((func.col("result") == "WIN").cast(IntegerType())).alias("won_count"),
-                func.count("result").alias("games_count"),
-            )
-            .withColumn("win_ratio", func.col("won_count") / func.col("games_count"))
-            .select("this_team_api_id", "win_ratio")
+        transformed_match_df.groupBy("this_team_api_id")
+        .agg(
+            func.sum((func.col("result") == "WIN").cast(IntegerType())).alias(
+                "won_count"
+            ),
+            func.count("result").alias("games_count"),
+        )
+        .withColumn("win_ratio", func.col("won_count") / func.col("games_count"))
+        .select("this_team_api_id", "win_ratio")
     )
 
+
 def get_match_players(match_df: DataFrame):
-    def match_player_df(df, type_of_team: str, is_home:bool):
+    def match_player_df(df, type_of_team: str, is_home: bool):
         return df.select(
-            'match_api_id',
-            func.explode(func.array(*tuple(f'{type_of_team}_player_{item}' for item in range(1, 12)))).alias(
-                "player_api_id")
-        ).withColumn('is_home_player', func.lit(is_home))
+            "match_api_id",
+            func.explode(
+                func.array(
+                    *tuple(f"{type_of_team}_player_{item}" for item in range(1, 12))
+                )
+            ).alias("player_api_id"),
+        ).withColumn("is_home_player", func.lit(is_home))
 
     home_match_player_df = match_player_df(match_df, "home", True)
     away_match_player_df = match_player_df(match_df, "away", False)
@@ -70,34 +85,42 @@ def get_match_players(match_df: DataFrame):
 
 
 def get_player_stats(
-        match_player_df: DataFrame,
-        player_attributes_df: DataFrame,
-        high_potential_threshold: float = 0.8
-    ) -> DataFrame:
-    average_potential_df = (
-        player_attributes_df
-            .groupBy("player_api_id")
-            .agg(
-                func.mean("potential").alias("potential")
-            )
+    match_player_df: DataFrame,
+    player_attributes_df: DataFrame,
+    high_potential_threshold: float = 0.8,
+) -> DataFrame:
+    average_potential_df = player_attributes_df.groupBy("player_api_id").agg(
+        func.mean("potential").alias("potential")
     )
     return (
-        match_player_df
-            .join(average_potential_df, "player_api_id", "leftouter")
-            .groupBy("match_api_id", "is_playing_home_game")
-            .agg(
-                func.max("potential").alias("max_potential"),
-                func.avg("potential").alias("average_potential"),
-            )
-            .withColumn("has_high_potential_player", func.col("max_potential") > high_potential_threshold)
-            .select("match_api_id", "is_playing_home_game", "has_high_potential_player", "average_potential")
+        match_player_df.join(average_potential_df, "player_api_id", "leftouter")
+        .groupBy("match_api_id", "is_playing_home_game")
+        .agg(
+            func.max("potential").alias("max_potential"),
+            func.avg("potential").alias("average_potential"),
+        )
+        .withColumn(
+            "has_high_potential_player",
+            func.col("max_potential") > high_potential_threshold,
+        )
+        .select(
+            "match_api_id",
+            "is_playing_home_game",
+            "has_high_potential_player",
+            "average_potential",
+        )
     )
-
 
 
 def get_average_player_potential(match_df, player_attribute_df):
     match_player_df = get_match_player_df(match_df)
-    averaged_player_attributes = player_attribute_df.select(
-        "player_api_id", func.col("potential").cast(FloatType())
-    ).groupBy("player_api_id").agg(func.mean("potential").alias("player_average_potential"))
-    return match_player_df.join(averaged_player_attributes, "player_api_id", "leftouter")
+    averaged_player_attributes = (
+        player_attribute_df.select(
+            "player_api_id", func.col("potential").cast(FloatType())
+        )
+        .groupBy("player_api_id")
+        .agg(func.mean("potential").alias("player_average_potential"))
+    )
+    return match_player_df.join(
+        averaged_player_attributes, "player_api_id", "leftouter"
+    )
--- tests/integration/test_transform.py
+++ tests/integration/test_transform.py
@@ -6,67 +6,192 @@ from src.jobs.transform import get_player_stats, get_team_win_ratio, get_match_p
 
 
 def test_get_match_players(spark_session_test: SparkSession) -> None:
-    match_1_home_players = dict(zip([f"home_player_{i}" for i in range(1, 12)], range(1, 12)))
-    match_1_away_players = dict(zip([f"away_player_{i}" for i in range(1, 12)], range(12, 23)))
-    match_2_home_players = dict(zip([f"home_player_{i}" for i in range(1, 12)], range(10, 21)))
-    match_2_away_players = dict(zip([f"away_player_{i}" for i in range(1, 12)], range(20, 31)))
-    match_df = spark_session_test.createDataFrame([
-        Row(match_api_id=1, **match_1_home_players, **match_1_away_players),
-        Row(match_api_id=2, **match_2_home_players, **match_2_away_players),
-    ])
+    match_1_home_players = dict(
+        zip([f"home_player_{i}" for i in range(1, 12)], range(1, 12))
+    )
+    match_1_away_players = dict(
+        zip([f"away_player_{i}" for i in range(1, 12)], range(12, 23))
+    )
+    match_2_home_players = dict(
+        zip([f"home_player_{i}" for i in range(1, 12)], range(10, 21))
+    )
+    match_2_away_players = dict(
+        zip([f"away_player_{i}" for i in range(1, 12)], range(20, 31))
+    )
+    match_df = spark_session_test.createDataFrame(
+        [
+            Row(match_api_id=1, **match_1_home_players, **match_1_away_players),
+            Row(match_api_id=2, **match_2_home_players, **match_2_away_players),
+        ]
+    )
     out_df = get_match_players(match_df)
     assert sorted(out_df.collect()) == sorted(
-        [Row(match_api_id=1, player_api_id=player_id, is_playing_home_game=True) for player_id in match_1_home_players.values()]
-        + [Row(match_api_id=1, player_api_id=player_id, is_playing_home_game=False) for player_id in match_1_away_players.values()]
-        + [Row(match_api_id=2, player_api_id=player_id, is_playing_home_game=True) for player_id in match_2_home_players.values()]
-        + [Row(match_api_id=2, player_api_id=player_id, is_playing_home_game=False) for player_id in match_2_away_players.values()]
+        [
+            Row(match_api_id=1, player_api_id=player_id, is_playing_home_game=True)
+            for player_id in match_1_home_players.values()
+        ]
+        + [
+            Row(match_api_id=1, player_api_id=player_id, is_playing_home_game=False)
+            for player_id in match_1_away_players.values()
+        ]
+        + [
+            Row(match_api_id=2, player_api_id=player_id, is_playing_home_game=True)
+            for player_id in match_2_home_players.values()
+        ]
+        + [
+            Row(match_api_id=2, player_api_id=player_id, is_playing_home_game=False)
+            for player_id in match_2_away_players.values()
+        ]
     )
 
 
 def test_get_team_win_ratio(spark_session_test: SparkSession) -> None:
-    normalized_match_df = spark_session_test.createDataFrame([
-        Row(match_api_id=1, this_team_api_id="team1", other_team_api_id="team2", result="WIN"),
-        Row(match_api_id=1, this_team_api_id="team2", other_team_api_id="team1", result="LOSE"),
-        Row(match_api_id=2, this_team_api_id="team1", other_team_api_id="team3", result="WIN"),
-        Row(match_api_id=2, this_team_api_id="team3", other_team_api_id="team1", result="LOSE"),
-        Row(match_api_id=3, this_team_api_id="team2", other_team_api_id="team3", result="TIE"),
-        Row(match_api_id=3, this_team_api_id="team3", other_team_api_id="team2", result="TIE"),
-        Row(match_api_id=4, this_team_api_id="team3", other_team_api_id="team4", result="LOSE"),
-        Row(match_api_id=4, this_team_api_id="team4", other_team_api_id="team3", result="WIN"),
-        Row(match_api_id=5, this_team_api_id="team3", other_team_api_id="team4", result="WIN"),
-        Row(match_api_id=5, this_team_api_id="team4", other_team_api_id="team3", result="LOSE"),
-    ])
+    normalized_match_df = spark_session_test.createDataFrame(
+        [
+            Row(
+                match_api_id=1,
+                this_team_api_id="team1",
+                other_team_api_id="team2",
+                result="WIN",
+            ),
+            Row(
+                match_api_id=1,
+                this_team_api_id="team2",
+                other_team_api_id="team1",
+                result="LOSE",
+            ),
+            Row(
+                match_api_id=2,
+                this_team_api_id="team1",
+                other_team_api_id="team3",
+                result="WIN",
+            ),
+            Row(
+                match_api_id=2,
+                this_team_api_id="team3",
+                other_team_api_id="team1",
+                result="LOSE",
+            ),
+            Row(
+                match_api_id=3,
+                this_team_api_id="team2",
+                other_team_api_id="team3",
+                result="TIE",
+            ),
+            Row(
+                match_api_id=3,
+                this_team_api_id="team3",
+                other_team_api_id="team2",
+                result="TIE",
+            ),
+            Row(
+                match_api_id=4,
+                this_team_api_id="team3",
+                other_team_api_id="team4",
+                result="LOSE",
+            ),
+            Row(
+                match_api_id=4,
+                this_team_api_id="team4",
+                other_team_api_id="team3",
+                result="WIN",
+            ),
+            Row(
+                match_api_id=5,
+                this_team_api_id="team3",
+                other_team_api_id="team4",
+                result="WIN",
+            ),
+            Row(
+                match_api_id=5,
+                this_team_api_id="team4",
+                other_team_api_id="team3",
+                result="LOSE",
+            ),
+        ]
+    )
     out_df = get_team_win_ratio(normalized_match_df)
-    assert sorted(out_df.collect()) == sorted([
-        Row(team_api_id="team1", win_ratio=1.0),
-        Row(team_api_id="team2", win_ratio=0.0),
-        Row(team_api_id="team3", win_ratio=0.25),
-        Row(team_api_id="team4", win_ratio=0.5),
-    ])
-
+    assert sorted(out_df.collect()) == sorted(
+        [
+            Row(team_api_id="team1", win_ratio=1.0),
+            Row(team_api_id="team2", win_ratio=0.0),
+            Row(team_api_id="team3", win_ratio=0.25),
+            Row(team_api_id="team4", win_ratio=0.5),
+        ]
+    )
 
 
 def test_get_player_stats(spark_session_test: SparkSession) -> None:
-    match_player_df = spark_session_test.createDataFrame([
-        Row(match_api_id="match1", player_api_id="player1", is_playing_home_game=True),
-        Row(match_api_id="match1", player_api_id="player2", is_playing_home_game=True),
-        Row(match_api_id="match1", player_api_id="player3", is_playing_home_game=False),
-        Row(match_api_id="match1", player_api_id="player4", is_playing_home_game=False),
-        Row(match_api_id="match2", player_api_id="player1", is_playing_home_game=True),
-        Row(match_api_id="match2", player_api_id="player2", is_playing_home_game=False),
-    ])
-    player_attributes_df = spark_session_test.createDataFrame([
-        Row(player_api_id="player1", date=date(2021, 1, 1), potential=0.9),
-        Row(player_api_id="player2", date=date(2021, 1, 1), potential=0.7),
-        Row(player_api_id="player2", date=date(2021, 1, 2), potential=0.8),
-        Row(player_api_id="player3", date=date(2021, 1, 1), potential=0.7),
-        Row(player_api_id="player4", date=date(2021, 1, 1), potential=0.8),
-        Row(player_api_id="player4", date=date(2021, 1, 2), potential=1.0),
-    ])
+    match_player_df = spark_session_test.createDataFrame(
+        [
+            Row(
+                match_api_id="match1",
+                player_api_id="player1",
+                is_playing_home_game=True,
+            ),
+            Row(
+                match_api_id="match1",
+                player_api_id="player2",
+                is_playing_home_game=True,
+            ),
+            Row(
+                match_api_id="match1",
+                player_api_id="player3",
+                is_playing_home_game=False,
+            ),
+            Row(
+                match_api_id="match1",
+                player_api_id="player4",
+                is_playing_home_game=False,
+            ),
+            Row(
+                match_api_id="match2",
+                player_api_id="player1",
+                is_playing_home_game=True,
+            ),
+            Row(
+                match_api_id="match2",
+                player_api_id="player2",
+                is_playing_home_game=False,
+            ),
+        ]
+    )
+    player_attributes_df = spark_session_test.createDataFrame(
+        [
+            Row(player_api_id="player1", date=date(2021, 1, 1), potential=0.9),
+            Row(player_api_id="player2", date=date(2021, 1, 1), potential=0.7),
+            Row(player_api_id="player2", date=date(2021, 1, 2), potential=0.8),
+            Row(player_api_id="player3", date=date(2021, 1, 1), potential=0.7),
+            Row(player_api_id="player4", date=date(2021, 1, 1), potential=0.8),
+            Row(player_api_id="player4", date=date(2021, 1, 2), potential=1.0),
+        ]
+    )
     out_df = get_player_stats(match_player_df, player_attributes_df)
-    assert sorted(out_df.collect()) == sorted([
-        Row(match_api_id="match1", is_playing_home_game=True, has_high_potential_player=True, average_potential=(0.9 + 0.75)/2),
-        Row(match_api_id="match1", is_playing_home_game=False, has_high_potential_player=True, average_potential=(0.7+0.9)/2),
-        Row(match_api_id="match2", is_playing_home_game=True, has_high_potential_player=True, average_potential=0.9),
-        Row(match_api_id="match2", is_playing_home_game=False, has_high_potential_player=False, average_potential=0.75),
-    ])
+    assert sorted(out_df.collect()) == sorted(
+        [
+            Row(
+                match_api_id="match1",
+                is_playing_home_game=True,
+                has_high_potential_player=True,
+                average_potential=(0.9 + 0.75) / 2,
+            ),
+            Row(
+                match_api_id="match1",
+                is_playing_home_game=False,
+                has_high_potential_player=True,
+                average_potential=(0.7 + 0.9) / 2,
+            ),
+            Row(
+                match_api_id="match2",
+                is_playing_home_game=True,
+                has_high_potential_player=True,
+                average_potential=0.9,
+            ),
+            Row(
+                match_api_id="match2",
+                is_playing_home_game=False,
+                has_high_potential_player=False,
+                average_potential=0.75,
+            ),
+        ]
+    )
